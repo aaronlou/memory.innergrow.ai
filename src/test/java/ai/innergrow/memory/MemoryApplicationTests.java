20e143a9654fe3d2dbb46ce30c7c353d4d623cbb
lou_.5@aliyun.com <siyuanlou1986@petalmail.com>
2026-01-17 08:24:27 +0800

    first commit

---
+++ src/test/java/ai/innergrow/memory/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.innergrow.memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
